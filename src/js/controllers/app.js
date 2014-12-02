70416bc8c21cab143d8cc5f7af7027a371545c08
Hudson Marinho <hudsonmarinhodev@gmail.com>
2014-12-02 15:14:41 -0300

    refactored js

--- src/js/controllers/app.js
+++ src/js/controllers/app.js
@@ -1,35 +1,67 @@
-angular.module('leroyApp', [])
-  .controller('ShoppingCart', ['$scope', function($scope) {
-    $total = 77636;
-    $minCard = 10000;
-    $maxCard = 60000;
-
-    $scope.buy = {};
-    $scope.buy.total = $total;
-    $scope.buy.cardOne = $total;
-
-    if($scope.buy.total >= $minCard) {
-      $scope.twoCards = true;
-      $scope.buy.cardTwo = 0;
+angular.module('leroyApp', []).controller('ShoppingCart', ['$scope', function($scope) {
+  $scope.check_cards = function ($total, $min, $cards) {
+    if($total > $min) {
+      $scope.two_cards = true;
+      $cards.card_two.totalValue = 0;
+    } else {
+      $scope.two_cards = false;
+      $cards.card_two.totalValue = 0;
     }
+  }
 
-    $scope.$watch('buy.total', function (value) {
-      $scope.$watch('buy.cardOne', function (value) {
-        cardOneValue = $scope.buy.cardOne;
+  $scope.check_installments = function (total, min, max, total_installment_max) {
+    var total_min = Math.floor(total / min);
 
-        if(cardOneValue >= $total) {
-          $scope.buy.cardOne = $total;
-        }
-      });
+    if(total <= max){
+      return (total_min == 0 && 1);
+    } else {
+      return total_installment_max;
+    }
+  }
 
-      $scope.$watch('buy.cardTwo', function (value) {
-        cardTwoValue = $scope.buy.cardTwo;
+  var prince_total     = 77636,
+      card_min         = 10000,
+      card_max         = 60000,
+      installment_min  = 1,
+      installment_max  = 6;
 
-        if(cardTwoValue >= $total) {
-          $scope.buy.cardTwo = $total;
-        }
+  $scope.buy = {
+    total : prince_total,
+    cards : {
+      card_one : {
+        total : prince_total,
+        installments : 1
+      },
+      card_two : {
+        total : 0,
+        installments : 0
+      }
+    }
+  }
 
-        $scope.buy.cardOne = $total - cardTwoValue;
-      });
-    });
+  $scope.check_cards(prince_total, card_min, $scope.buy.cards);
+
+  $scope.$watch('buy.total', function (value) {
+    $scope.check_cards(value, card_min, $scope.buy.cards);
+    $scope.buy.cards.card_one.total = value - $scope.buy.cards.card_two.total;
+  });
+
+  $scope.$watch('buy.cards.card_two.total', function (value) {
+    var total_one = $scope.buy.total - $scope.buy.cards.card_two.total;
+
+    if($scope.buy.cards.card_two.total > $scope.buy.total) {
+      $scope.buy.cards.card_one.total = 0;
+      $scope.buy.cards.card_two.total = $scope.buy.total;
+
+    } else {
+      $scope.buy.cards.card_one.total = total_one;
+      $scope.buy.cards.card_two.total = value;
+
+      $scope.buy.cards.card_one.installments = $scope.check_installments(total_one, card_min, card_max, installment_max);
+      $scope.buy.cards.card_two.installments = $scope.check_installments(value, card_min, card_max, installment_max);
+
+      console.log($scope.buy.cards.card_one.installments)
+      console.log($scope.buy.cards.card_two.installments)
+    }
+  });
 }]);
